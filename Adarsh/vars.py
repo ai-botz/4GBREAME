f9cb6ed714d22f66da2582b79381191f9eb51b80
HAROON AI REPO 25 <93036340+ai-botz@users.noreply.github.com>
2023-11-24 22:54:35 +0500

    vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -9,23 +9,23 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID'))
-    API_HASH = str(getenv('API_HASH'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN'))
+    API_ID = int(getenv('25443947'))
+    API_HASH = str(getenv('ab4cd800dac7c9a36314ee83800adba8'))
+    BOT_TOKEN = str(getenv('6821223808:AAEZsS3AA3PlV3esA9VrZStEL_nSRLJs3ac'))
     name = str(getenv('name', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL'))
+    BIN_CHANNEL = int(getenv('-1002144427738'))
     PORT = int(getenv('PORT', 8080))
-    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
+    BIND_ADRESS = str(getenv('mongodb+srv://haroonhassan:@27892789@@cluster0.djfzdhq.mongodb.net/?retryWrites=true&w=majority', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("6013614984", "").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME'))
+    OWNER_USERNAME = str(getenv('haroon_ai'))
     if 'DYNO' in environ:
         ON_HEROKU = True
-        APP_NAME = str(getenv('APP_NAME'))
+        APP_NAME = str(getenv('AI_BOTZ'))
     
     else:
         ON_HEROKU = False
