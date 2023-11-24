a0df6f16f1fd4ab7004ade42d96a033f21fb030b
HAROON AI REPO 25 <93036340+ai-botz@users.noreply.github.com>
2023-11-24 22:57:57 +0500

    __init__.py

--- Adarsh/bot/__init__.py
+++ Adarsh/bot/__init__.py
@@ -6,9 +6,9 @@ from os import getcwd
 
 StreamBot = Client(
     name='Web Streamer',
-    api_id=Var.API_ID,
-    api_hash=Var.API_HASH,
-    bot_token=Var.BOT_TOKEN,
+    api_id=Var.25443947,
+    api_hash=Var.ab4cd800dac7c9a36314ee83800adba8,
+    bot_token=Var.6821223808:AAEZsS3AA3PlV3esA9VrZStEL_nSRLJs3ac,
     sleep_threshold=Var.SLEEP_THRESHOLD,
     workers=Var.WORKERS
 )
